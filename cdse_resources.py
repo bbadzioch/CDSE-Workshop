1ea60c5fd6b27e60f452abc897d89e3898b62d7c
bbadzioch <badzioch@buffalo.edu>
2019-03-05 16:15:10 -0500

    resources

--- cdse_resources.py
+++ cdse_resources.py
@@ -73,4 +73,5 @@ workshop Piazza page:
 piazza.com/buffalo/spring2019/cdsepython''')
 else:
     cc = importlib.import_module(local_dir + '.' +  "cdse_copy_cells")
-    cc.C(f"0 -f {local_dir}/{flist[1]}")
+    s = f"0 -f {local_dir}/{flist[1]}"
+    cc.C(s)
